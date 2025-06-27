8b2227e6d11f4cac9314122480a0351d424fdb59
pankaj saindane <pankajsaindane40@gmail.com>
2025-06-28 00:30:18 +0530

    custom api error handling

--- src/index.js
+++ src/index.js
@@ -11,7 +11,14 @@ dotenv.config({
 })
 
 connectDB ()
-
+.then(()=>{
+     app.listen(process.env.PORT || 3000,()=>{
+        console.log(`App is listening on port ${process.env.PORT || 3000}`);
+     })
+})
+.catch((error)=>{
+    console.log("Error occurred from DB connection",error)
+})
 
 
 
